7a095ea4fcd45aada3d1eee42c9edce73d6a3345
ganesh <ganesh@kanexy.com>
2026-05-06 11:59:57 +0530

    updated to allure reports

--- playwright.config.js
+++ playwright.config.js
@@ -1,39 +1,34 @@
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-
   testDir: './tests',
 
   reporter: [
     ['list'],
-    ['allure-playwright']
+    ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
 
- retries: 0, // global default OFF
+  retries: 0,
 
   use: {
-  headless: true,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
 
-
-
-    projects: [
-      {
-        name: 'normal-tests',
-        testMatch: /.*\.spec\.js/,
-        grepInvert: /@flaky/,
-        retries: 1,
-      },
-      {
-        name: 'flaky-tests',
-        testMatch: /.*\.spec\.js/,
-        grep: /@flaky/,
-        retries: 2,
-      }
-    ]
-
+  projects: [
+    {
+      name: 'normal-tests',
+      testMatch: /.*\.spec\.js/,
+      grepInvert: /@flaky/,
+      retries: 1,
+    },
+    {
+      name: 'flaky-tests',
+      testMatch: /.*\.spec\.js/,
+      grep: /@flaky/,
+      retries: 2,
+    }
+  ]
 });
